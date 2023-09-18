1f3ead8fcd1e2e69f28cc3460d9653f0c33ceffb
Koketso Malope <imogiantkoketso@gmail.com>
2023-09-18 21:53:30 +0200

    Commit changes to save progress

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ArsernalComponent } from './arsernal/arsernal.component';
+import { BlogComponent } from './blog/blog.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { BioComponent } from './bio/bio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'arsernal', component: ArsernalComponent },
+  { path: 'blog', component: BlogComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'bio', component: BioComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavComponent } from './nav/nav.component';
+import { BlogComponent } from './blog/blog.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ArsernalComponent } from './arsernal/arsernal.component';
+import { BioComponent } from './bio/bio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavComponent,
+    BlogComponent,
+    ProjectsComponent,
+    ArsernalComponent,
+    BioComponent
   ],
   imports: [
     BrowserModule,
