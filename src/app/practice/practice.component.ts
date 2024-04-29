28347aa4ec0d91b023a968ab48ec6e1feecea3c2
Koketso Malope <imogiantkoketso@gmail.com>
2024-04-29 21:45:19 +0200

    new components and enhancements

--- src/app/practice/practice.component.ts
+++ src/app/practice/practice.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+interface User {
+  id: number;
+  name: string;
+  email: string
+}
 
 @Component({
   selector: 'app-practice',
   templateUrl: './practice.component.html',
   styleUrls: ['./practice.component.scss']
 })
-export class PracticeComponent {
+export class PracticeComponent implements OnInit {
+
+  users: User[] = [];
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.http.get<User[]>('https://example.com/api/users')
+      .subscribe(data => this.users = data);
+
+  }
 
 }
